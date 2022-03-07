31d857ffeef4716ce8d240a062227e1fa8f70f68
Obolduy <ivanox01@yandex.ru>
2022-03-07 11:34:00 +0300

    Props в QuoteList

--- src/components/QuotesList.vue
+++ src/components/QuotesList.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-for="obj in massive" v-bind:key="obj.id">
+  <div v-for="quote in quotes" v-bind:key="quote.id">
     <v-card
     class="mx-auto mt-2"
     max-width="70%"
@@ -9,12 +9,12 @@
       <v-card-header>
         <div>
           <div class="text-overline mb-1">
-            {{obj.author}}
+            {{quote.author}}
           </div>
           <div class="text-h6 mb-1">
-            <span>{{obj.quote}}</span> 
+            <span>{{quote.quote}}</span> 
           </div>
-          <div class="text-caption">{{obj.tags}}</div>
+          <div class="text-caption">{{quote.tags}}</div>
         </div>
       </v-card-header>
     </v-card>
@@ -23,31 +23,25 @@
 
 <script>
   export default {
+    props: {
+      quotes: {
+          type: Array,
+          required: true
+      }
+    },
     data() {
       return {
-        massive: [
+        quotes: [
           {id: 1, author: "Joker", quote: "Не тот джокер кто не джокер а тот кто джокер", tags: "Цитаты джокера", date: "15.10.2003"},
           {id: 2, author: "Владик", quote: "Раз два три четыре пять", tags: "Цитаты владика, цитаты владика 2", date: "15.10.2003"},
           {id: 3, author: "мне лень думать 1", quote: "ТУТ МОЖЕТ БЫТЬ И БОЛЕЕ ДЛИННАЯ ЦИТАТА", tags: "Цитаты джокера 2", date: "15.10.2003"},
           {id: 4, author: "мне лень думать 2", quote: "Я НЕ ЗНАЮ КАКОЙ ЛИМИТ НА САМОМ ДЕЛЕ", tags: "Тут до трех тегов если что", date: "15.10.2003"},
           {id: 5, author: "ПрИвЕт Из 2007", quote: "Абоба", tags: "Цитаты неджокера", date: "15.10.2003"},
         ],
-        name: '',
-        newname: '',
       }
     },
     methods: {
-      testFunc() {
-        const testInterest = {
-          id: this.massive.length + 1,
-          biba: this.name,
-          boba: this.newname
-        }
-
-        this.name = '';
-        this.newname = '';
-        this.massive.push(testInterest)
-      }
+        
     }
   }
 </script>

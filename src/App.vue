f70c72b33bc784650c06f55babac36ca95837dd3
Obolduy <ivanox01@yandex.ru>
2022-03-07 11:33:14 +0300

    Подписка на событие добавление поста в App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
   <header-green/>
-  <quotes-list/>
+  <add-form @newQuote="addQuote"/>
+  <quotes-list :quotes="quotes"/>
 </template>
 
 <script>
   import HeaderGreen from "@/components/HeaderGreen.vue";
   import QuotesList from "@/components/QuotesList.vue";
+  import AddForm from "@/components/AddForm.vue";
 
   export default {
     components: {
-      HeaderGreen, QuotesList
+      HeaderGreen, AddForm, QuotesList
+    },
+    methods: {
+      addQuote(newQuote) {
+        console.log(newQuote)
+      }
     }
-
   }
 </script>
 

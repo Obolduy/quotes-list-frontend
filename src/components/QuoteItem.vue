e18de1717c3a35190cff287cfd9983a0985aad0d
Obolduy <ivanox01@yandex.ru>
2022-03-08 21:57:39 +0300

    Отображение полной информации о цитате

--- src/components/QuoteItem.vue
+++ src/components/QuoteItem.vue
@@ -4,6 +4,7 @@
     width="70%"
     variant="outlined"
     color="#5f7a59"
+    @click="showQuote"
   >
     <v-card-header>
       <div>
@@ -18,17 +19,38 @@
         </div>
       </div>
     </v-card-header>
-</v-card>
+  </v-card>
+  <single-quote-shower v-model:show="quoteVisible">
+    <single-quote
+    :quote="post"
+    />
+  </single-quote-shower>
 </template>
 
 <script>
+import SingleQuoteShower from "@/components/SingleQuoteShower.vue";
+import SingleQuote from "@/components/SingleQuote.vue";
+
 export default {
+  components: {
+    SingleQuoteShower, SingleQuote
+  },
+  data() {
+    return {
+      quoteVisible: false,
+    }
+  },
   props: {
     post: {
       type: Object,
       required: true
     }
-  }
+  },
+  methods: {
+    showQuote() {
+      this.quoteVisible = true;
+    }
+  },
 }
 </script>
 
---
+++ src/components/SingleQuote.vue
@@ -0,0 +1,54 @@
+<template>
+    <v-card
+    class="mx-auto mt-2"
+    variant="outlined"
+    color="#5f7a59"
+    width="70%"
+    style="background-color: white; border-radius: 23px; overflow-x: auto;"
+    @click="showQuote"
+    elevation="24"
+    >
+    <v-card-header>
+      <div>
+        <div class="text-h1 mb-4">
+          <span id="quote">„{{quote.text}}“</span> 
+        </div>
+        <div class="text-h5 mb-1">
+          Автор: {{quote.author}}
+        </div>
+        <div class="text-h6">
+          Теги: <span id="tags">{{quote.tags}}</span>
+        </div>
+      </div>
+    </v-card-header>
+    <v-card-subtitle class="pb-2">
+      Дата добавления: {{time}}
+    </v-card-subtitle>
+  </v-card>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            time: new Date(this.quote.created_at).toLocaleDateString("ru-RU"),
+        }
+    },
+    props: {
+        quote: {
+            type: Object
+        }
+    }
+}
+</script>
+
+<style scoped>
+    #quote {
+        font-style: italic;
+        font-weight: 400;
+    }
+    #tags {
+        font-style: italic;
+        font-weight: 400;
+    }
+</style>
